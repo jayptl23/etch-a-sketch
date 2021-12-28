79c451ec111f5f061f247e1a8a244b01c44b3c8a
Jay Patel <29896894+jayptl23@users.noreply.github.com>
2021-12-28 16:59:08 -0500

    display basic grid

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-document.querySelector('.canvas').textContent = 'Hello World'
+const displayController = (function () {
+	const canvas = document.querySelector('.canvas')
+
+	function setup() {
+		canvas.classList.add('small')
+		generateGrid()
+	}
+
+	function generateGrid() {
+		for (let i = 0; i < 8; i++) {
+			for (let j = 0; j < 8; j++) {
+				// make a new tile and append to canvas
+				const tile = document.createElement('div')
+				tile.style.border = '1px solid black'
+				canvas.appendChild(tile)
+			}
+		}
+	}
+
+	return {
+		setup,
+	}
+})()
+
+displayController.setup()
